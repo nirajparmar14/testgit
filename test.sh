5cdd3eb1a92d7b0bda695f36414a4c5b7c219686
nirajparmar14 <48626593+nirajparmar14@users.noreply.github.com>
2020-09-03 13:49:24 +0000

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,9 +1,10 @@
+MORN="False"
 echo "In Script"
 ls -1 $AGENT_WORKFOLDER
 echo ${BUILD_BUILDNUMBER}
 pwd
 echo "AFter PWD"
 ls -l
-sleep 10m
+echo "##vso[task.setvariable variable=MORN;isOutput=true]$MORN"
 echo Add other tasks to build, test, and deploy your project.
 echo "See https://aka.ms/yaml"
