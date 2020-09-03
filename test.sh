803e3a798e4535cf2219f138fd3161076f6ae554
nirajparmar14 <48626593+nirajparmar14@users.noreply.github.com>
2020-09-03 12:31:58 +0000

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,3 +1,9 @@
 echo "In Script"
 ls -1 $AGENT_WORKFOLDER
 echo ${BUILD_BUILDNUMBER}
+pwd
+echo "AFter PWD"
+ls -l
+sleep 10m
+echo Add other tasks to build, test, and deploy your project.
+echo "See https://aka.ms/yaml"
