d6941ba3d1ff1fb848c41916c19f7da0da3af743
nirajparmar14 <48626593+nirajparmar14@users.noreply.github.com>
2020-09-03 13:58:05 +0000

    Update test1.sh

--- test1.sh
+++ test1.sh
@@ -1,3 +1,4 @@
+Testing=$1 
 echo "In Script"
 ls -1 $AGENT_WORKFOLDER
 echo ${BUILD_BUILDNUMBER}
@@ -7,3 +8,8 @@ ls -l
 sleep 1m
 echo Add other tasks to build, test, and deploy your project.
 echo "See https://aka.ms/yaml"
+if [ $Testing == "ABC" ]; then
+  echo $Testing
+else
+  exit 1
+fi
